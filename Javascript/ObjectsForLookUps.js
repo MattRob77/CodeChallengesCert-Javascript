25f77ddbb5e40d55275453cfe4d1aa4944ea717e
MattRob77 <Matt_Robinson1@yahoo.com>
2020-05-11 14:45:06 -0400

    Updated coding challenge with solution.

--- Javascript/ObjectsForLookUps.js
+++ Javascript/ObjectsForLookUps.js
@@ -55,3 +55,25 @@ phoneticLookup("charlie");
 
 
 solution:
+// Setup
+function phoneticLookup(val) {
+  var result = "";
+
+  var lookup = {
+    "alpha": "Adams",
+    "bravo": "Boston",
+    "charlie": "Chicago",
+    "delta": "Denver",
+    "echo": "Easy",
+    "foxtrot": "Frank"
+  };
+  // Only change code below this line
+
+  result = lookup[val];
+
+  // Only change code above this line
+  return result;
+}
+
+// Change this value to test
+phoneticLookup("charlie");
