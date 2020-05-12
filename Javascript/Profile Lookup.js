70c847416555d9993adbcc88ebf93fa9935a6fad
MattRob77 <Matt_Robinson1@yahoo.com>
2020-05-12 16:46:19 -0400

    Implemented solution for challenge with comments explaining the code.

--- Javascript/Profile Lookup.js
+++ Javascript/Profile Lookup.js
@@ -53,3 +53,17 @@ lookUpProfile("Akira", "likes");
 
 
 solution:
+function lookUpProfile(name, prop) {
+  for (var x = 0; x < contacts.length; x++) {
+    if (contacts[x].firstName === name) {
+      if (contacts[x].hasOwnProperty(prop)) {
+        return contacts[x][prop];
+      } else {
+        return "No such property";
+      }
+    }
+  }
+  return "No such contact";
+}
+
+lookUpProfile("Akira", "likes");
