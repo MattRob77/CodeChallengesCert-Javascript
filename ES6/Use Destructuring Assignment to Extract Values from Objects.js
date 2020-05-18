bf7b0eee836b2a57ee9bdf26bb2028f068fcf2ed
MattRob77 <Matt_Robinson1@yahoo.com>
2020-05-18 15:27:34 -0400

    Implemeted coding solution using destructuring.

--- ES6/Use Destructuring Assignment to Extract Values from Objects.js
+++ ES6/Use Destructuring Assignment to Extract Values from Objects.js
@@ -16,3 +16,17 @@
 // You can extract as many or few values from the object as you want.
 //
 // Replace the two assignments with an equivalent destructuring assignment. It should still assign the variables today and tomorrow the values of today and tomorrow from the HIGH_TEMPERATURES object.
+
+
+solution:
+const HIGH_TEMPERATURES = {
+  yesterday: 75,
+  today: 77,
+  tomorrow: 80
+};
+
+// Only change code below this line
+
+const {today, tomorrow} = HIGH_TEMPERATURES;//clean up code using destructuring.
+
+// Only change code above this line
