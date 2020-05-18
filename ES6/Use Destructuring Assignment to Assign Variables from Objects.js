c3d9edb836a02db214d6ea15de152efdd16a804a
MattRob77 <Matt_Robinson1@yahoo.com>
2020-05-18 15:33:13 -0400

    Implemented coding solution.

--- ES6/Use Destructuring Assignment to Assign Variables from Objects.js
+++ ES6/Use Destructuring Assignment to Assign Variables from Objects.js
@@ -27,4 +27,15 @@ const highTomorrow = HIGH_TEMPERATURES.tomorrow;
 // Only change code above this line
 
 
-solution: 
+solution:
+const HIGH_TEMPERATURES = {
+  yesterday: 75,
+  today: 77,
+  tomorrow: 80
+};
+
+// Only change code below this line
+
+const { today: highToday, tomorrow: highTomorrow } = HIGH_TEMPERATURES;
+
+// Only change code above this line
