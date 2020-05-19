e7b3022239591e2faae24d3c4ae5d6884dfca4d6
MattRob77 <Matt_Robinson1@yahoo.com>
2020-05-19 14:43:16 -0400

    Implemented coding solution using class component method.

--- ES6/Use class Syntax to Define a Constructor Function.js
+++ ES6/Use class Syntax to Define a Constructor Function.js
@@ -25,3 +25,17 @@
 // Use the class keyword and write a constructor to create the Vegetable class.
 //
 // The Vegetable class allows you to create a vegetable object with a property name that gets passed to the constructor.
+
+
+solution:
+// Only change code below this line
+class Vegetable { //used class method 
+  constructor(name) { //contructor with object property name
+    this.name = name
+  }
+}
+
+// Only change code above this line
+
+const carrot = new Vegetable('carrot');
+console.log(carrot.name); // Should display 'carrot'
