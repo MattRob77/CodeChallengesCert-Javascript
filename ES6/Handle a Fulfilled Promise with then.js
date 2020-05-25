9428aa65a4aa6e41945f40b64968a9c4c38c8a29
MattRob77 <Matt_Robinson1@yahoo.com>
2020-05-25 11:39:39 -0400

    Implemented solution using .then for promises.

--- ES6/Handle a Fulfilled Promise with then.js
+++ ES6/Handle a Fulfilled Promise with then.js
@@ -7,3 +7,19 @@
 // result comes from the argument given to the resolve method.
 //
 // Add the then method to your promise. Use result as the parameter of its callback function and log result to the console.
+
+solution:
+const makeServerRequest = new Promise((resolve, reject) => {
+  // responseFromServer is set to true to represent a successful response from a server
+  let responseFromServer = true;
+
+  if(responseFromServer) {
+    resolve("We got the data");
+  } else {
+    reject("Data not received");
+  }
+});
+
+makeServerRequest.then(result => {
+  console.log(result);
+});
