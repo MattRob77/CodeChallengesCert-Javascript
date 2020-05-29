4424c271b154f5919400b40ff599f49a80d66a0f
MattRob77 <Matt_Robinson1@yahoo.com>
2020-05-29 17:38:14 -0400

    Implented solution for coding challenge using + character.

--- RegularExpressions/Match Characters that Occur One or More Times.js
+++ RegularExpressions/Match Characters that Occur One or More Times.js
@@ -10,3 +10,6 @@
 // You want to find matches when the letter s occurs one or more times in "Mississippi". Write a regex that uses the + sign.
 
 solution:
+let difficultSpelling = "Mississippi";
+let myRegex = /s+/gi; 
+let result = difficultSpelling.match(myRegex);
