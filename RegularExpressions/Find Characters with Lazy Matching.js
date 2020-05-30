df232af746d15272fe52215cdde427c242764ba6
MattRob77 <Matt_Robinson1@yahoo.com>
2020-05-30 15:13:27 -0400

    Finished solution for Lazy Matching.

--- RegularExpressions/Find Characters with Lazy Matching.js
+++ RegularExpressions/Find Characters with Lazy Matching.js
@@ -12,4 +12,7 @@
 //
 // Fix the regex /<.*>/ to return the HTML tag <h1> and not the text "<h1>Winter is coming</h1>". Remember the wildcard . in a regular expression matches any character.
 
-solution: 
+solution:
+let text = "<h1>Winter is coming</h1>";
+let myRegex = /<.*?>/; 
+let result = text.match(myRegex);
