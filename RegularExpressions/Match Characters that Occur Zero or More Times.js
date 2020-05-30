07fed389afd38b063a3145f3979ae4d75f80f9bf
MattRob77 <Matt_Robinson1@yahoo.com>
2020-05-30 15:09:24 -0400

    Updated solution.

--- RegularExpressions/Match Characters that Occur Zero or More Times.js
+++ RegularExpressions/Match Characters that Occur Zero or More Times.js
@@ -13,3 +13,5 @@
 // For this challenge, chewieQuote has been initialized as "Aaaaaaaaaaaaaaaarrrgh!" behind the scenes. Create a regex chewieRegex that uses the * character to match an uppercase "A" character immediately followed by zero or more lowercase "a" characters in chewieQuote. Your regex does not need flags or character classes, and it should not match any of the other quotes.
 
 solution:
+let chewieRegex = /Aa*/;
+let result = chewieQuote.match(chewieRegex);
