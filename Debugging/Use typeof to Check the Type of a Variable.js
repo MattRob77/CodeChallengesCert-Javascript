ad17358c92be54546d2b2d636c295b659e073981
MattRob77 <Matt_Robinson1@yahoo.com>
2020-06-04 10:56:14 -0400

    Implemented solution for coding challenge.

--- Debugging/Use typeof to Check the Type of a Variable.js
+++ Debugging/Use typeof to Check the Type of a Variable.js
@@ -17,3 +17,9 @@ console.log(seven + three);
 // Only change code below this line
 
 solution:
+let seven = 7;
+let three = "3";
+console.log(seven + three);
+// Only change code below this line
+console.log(typeof seven);
+console.log(typeof three); 
