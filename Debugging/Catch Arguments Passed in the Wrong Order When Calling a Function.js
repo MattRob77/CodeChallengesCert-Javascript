143f49a2e1b350e237acf6a3261a8ff0b94c05c5
MattRob77 <Matt_Robinson1@yahoo.com>
2020-06-08 07:42:41 -0400

    Implented solution for challenge.

--- Debugging/Catch Arguments Passed in the Wrong Order When Calling a Function.js
+++ Debugging/Catch Arguments Passed in the Wrong Order When Calling a Function.js
@@ -11,3 +11,14 @@ let base = 2;
 let exp = 3;
 let power = raiseToPower(exp, base);
 console.log(power);
+
+
+solution:
+function raiseToPower(b, e) {
+  return Math.pow(b, e);
+}
+
+let base = 2;
+let exp = 3;
+let power = raiseToPower(base, exp);
+console.log(power);
