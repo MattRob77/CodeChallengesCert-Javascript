f3d45dd17e41b73af2c1bb2ec5581eb5d54fc457
MattRob77 <Matt_Robinson1@yahoo.com>
2020-06-10 10:58:15 -0400

    Implemnted solution using push and unshift.

--- DataStructures/Add Items to an Array with push() and unshift().js
+++ DataStructures/Add Items to an Array with push() and unshift().js
@@ -23,3 +23,12 @@ console.log(mixedNumbers(['IV', 5, 'six']));
 
 
 solution:
+function mixedNumbers(arr) {
+
+  arr.unshift('I', 2, 'three');
+  arr.push(7, 'VIII', 9);
+  
+  return arr;
+}
+
+console.log(mixedNumbers(['IV', 5, 'six']));
