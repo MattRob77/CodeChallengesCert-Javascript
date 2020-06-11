82df5ca96fb38bedd18ac5e9c09fb1127f89b6fc
MattRob77 <Matt_Robinson1@yahoo.com>
2020-06-11 08:43:18 -0400

    Implemented solution using splice method.

--- DataStructures/Add Items Using splice().js
+++ DataStructures/Add Items Using splice().js
@@ -24,3 +24,9 @@ console.log(htmlColorNames(['DarkGoldenRod', 'WhiteSmoke', 'LavenderBlush', 'Pal
 
 
 solution:
+function htmlColorNames(arr) {
+  arr.splice(0, 2, "DarkSalmon", "BlanchedAlmond");
+  return arr;
+}
+
+console.log(htmlColorNames(['DarkGoldenRod', 'WhiteSmoke', 'LavenderBlush', 'PaleTurquoise', 'FireBrick']));
