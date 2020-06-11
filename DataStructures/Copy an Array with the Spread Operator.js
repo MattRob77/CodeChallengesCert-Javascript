1830e60286a250bb634f4cc9f039146557f37c93
MattRob77 <Matt_Robinson1@yahoo.com>
2020-06-11 08:52:08 -0400

    Implemented solution for coding challenge using the spread operator.

--- DataStructures/Copy an Array with the Spread Operator.js
+++ DataStructures/Copy an Array with the Spread Operator.js
@@ -25,3 +25,15 @@ console.log(copyMachine([true, false, true], 2));
 
 
 solution:
+function copyMachine(arr, num) {
+  let newArr = [];
+  while (num >= 1) {
+
+  newArr.push([...arr]);
+    
+    num--;
+  }
+  return newArr;
+}
+
+console.log(copyMachine([true, false, true], 2));
