252e19b93c4589c4d5fcc0718895670b91401094
MattRob77 <Matt_Robinson1@yahoo.com>
2020-06-11 09:06:46 -0400

    Implemented coding solution.

--- DataStructures/Combine Arrays with the Spread Operator.js
+++ DataStructures/Combine Arrays with the Spread Operator.js
@@ -21,3 +21,10 @@ console.log(spreadOut());
 
 
 solution:
+function spreadOut() {
+  let fragment = ['to', 'code'];
+  let sentence = ["learning", ...fragment, "is", "fun"];
+  return sentence;
+}
+
+console.log(spreadOut());
