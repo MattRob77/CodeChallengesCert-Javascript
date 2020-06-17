763782867b529088b2d859d676d112147c726e88
MattRob77 <Matt_Robinson1@yahoo.com>
2020-06-17 14:08:26 -0400

    Added comments to help explain implementation.

--- CodeChallengesCert-Javascript/ Find the Longest Word in a String.js
+++ CodeChallengesCert-Javascript/ Find the Longest Word in a String.js
@@ -4,16 +4,16 @@
 // Your response should be a number.
 
 function findLongestWordLength(str) {
-  var words = str.split(' ');
-  var maxLength = 0;
+  var words = str.split(' '); //convert the string into an array
+  var maxLength = 0; //declare a variable of 0
 
-  for (var i = 0; i < words.length; i++) {
+  for (var i = 0; i < words.length; i++) { //compare the current word to the previous
     if (words[i].length > maxLength) {
-      maxLength = words[i].length;
+      maxLength = words[i].length; //storing the longest word
     }
   }
 
-  return maxLength;
+  return maxLength; //returns the variable maxLength 
 }
 
 
