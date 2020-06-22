8cfd8bc25061aabbd6cb06d407ed6d4648551f48
MattRob77 <Matt_Robinson1@yahoo.com>
2020-06-22 15:44:33 -0400

    Implented solution will add notes soon.

--- CodeChallengesCert-Javascript/ Slice and Splice.js
+++ CodeChallengesCert-Javascript/ Slice and Splice.js
@@ -8,10 +8,19 @@
 // Return the resulting array. The input arrays should remain the same after the function runs.
 
 function frankenSplice(arr1, arr2, n) {
-  return arr2;
+  var arr2Copy = arr2.slice();
+
 }
 
 frankenSplice([1, 2, 3], [4, 5, 6], 1);
 
 
 solution:
+function frankenSplice(arr1, arr2, n) {
+  	var arr2Copy = arr2.slice();
+	for (var i = 0; i < arr1.length; i++) {
+		arr2Copy.splice(n++, 0, arr1[i]);
+	}
+  return arr2Copy;
+}
+frankenSplice([1, 2, 3], [4, 5], 1)
