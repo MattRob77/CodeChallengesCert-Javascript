906fb3696190f10937ff2dae61201157a598e544
MattRob77 <Matt_Robinson1@yahoo.com>
2020-06-23 11:07:45 -0400

    Implemented coding solution.

--- CodeChallengesCert-Javascript/Mutations.js
+++ CodeChallengesCert-Javascript/Mutations.js
@@ -17,3 +17,10 @@ mutation(["hello", "hey"]);
 
 
 solution:
+function mutation(arr) {
+  for(var i = 0; i <= arr[1].length -1; i++){
+    if (arr[0].toLowerCase().indexOf(arr[1][i].toLowerCase()) == -1)
+    return false;
+  }
+    return true;
+}
