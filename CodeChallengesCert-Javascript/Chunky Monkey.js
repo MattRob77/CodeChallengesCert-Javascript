d40434f89365778848f600fd60231bd91e9df917
MattRob77 <Matt_Robinson1@yahoo.com>
2020-06-23 15:01:19 -0400

    Implemented coding solution needs to be refractured for the big o notation.

--- CodeChallengesCert-Javascript/Chunky Monkey.js
+++ CodeChallengesCert-Javascript/Chunky Monkey.js
@@ -8,3 +8,14 @@ chunkArrayInGroups(["a", "b", "c", "d"], 2);
 
 
 solution:
+function chunkArrayInGroups(arr, size) {
+ if(arr.length <= size) {
+   return [arr];
+ } else {
+   return [arr.slice(0, size)].concat (
+     chunkArrayInGroups(arr.slice(size), size)
+   );
+ }
+}
+
+chunkArrayInGroups(["a", "b", "c", "d"], 2);
