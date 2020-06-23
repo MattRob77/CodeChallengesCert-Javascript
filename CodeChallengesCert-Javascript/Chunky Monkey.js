0740bd7241c309291eaad94b05d4d4f5cef97399
MattRob77 <Matt_Robinson1@yahoo.com>
2020-06-23 15:02:58 -0400

    Added notes for implementation.

--- CodeChallengesCert-Javascript/Chunky Monkey.js
+++ CodeChallengesCert-Javascript/Chunky Monkey.js
@@ -8,9 +8,9 @@ chunkArrayInGroups(["a", "b", "c", "d"], 2);
 
 
 solution:
-function chunkArrayInGroups(arr, size) {
- if(arr.length <= size) {
-   return [arr];
+function chunkArrayInGroups(arr, size) { //created function
+ if(arr.length <= size) { //if array is smaller return nested
+   return [arr]; //larger array is split 
  } else {
    return [arr.slice(0, size)].concat (
      chunkArrayInGroups(arr.slice(size), size)
