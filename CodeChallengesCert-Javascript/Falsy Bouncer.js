fd0527c0218361defdc94cc72cdde6f990f4c9a7
MattRob77 <Matt_Robinson1@yahoo.com>
2020-06-23 10:43:47 -0400

    Added notes about the filter implementation.

--- CodeChallengesCert-Javascript/Falsy Bouncer.js
+++ CodeChallengesCert-Javascript/Falsy Bouncer.js
@@ -14,8 +14,8 @@ bouncer([7, "ate", "", false, 9]);
 
 
 solution:
-function bouncer(arr) {
-  return arr.filter(Boolean)
+function bouncer(arr) { //created function bouncer with one arg which is the array
+  return arr.filter(Boolean) //used .filter that creats and returns a new array with elements that pass the boolean test above
 }
 
 bouncer([7, "ate", "", false, 9]);
