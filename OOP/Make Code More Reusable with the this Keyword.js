68175b496a5a85ba4943c38c80f630364c7be354
MattRob77 <Matt_Robinson1@yahoo.com>
2020-07-04 17:30:02 -0400

    Implemented solution using this keyword.

--- OOP/Make Code More Reusable with the this Keyword.js
+++ OOP/Make Code More Reusable with the this Keyword.js
@@ -26,3 +26,11 @@ let dog = {
 dog.sayLegs();
 
 solution:
+let dog = {
+  name: "Spot",
+  numLegs: 4,
+  sayLegs: function()
+  {return "This dog has " + this.numLegs + " legs.";}
+};
+
+dog.sayLegs();
