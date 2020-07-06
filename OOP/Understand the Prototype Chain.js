15776c91d97baca4b8b244290f1af31700833ad5
MattRob77 <Matt_Robinson1@yahoo.com>
2020-07-06 12:38:34 -0400

    Implemented coding solution.

--- OOP/Understand the Prototype Chain.js
+++ OOP/Understand the Prototype Chain.js
@@ -31,3 +31,13 @@ Dog.prototype.isPrototypeOf(beagle);  // yields true
 
 
 solution:
+function Dog(name) {
+  this.name = name;
+}
+
+let beagle = new Dog("Snoopy");
+
+Dog.prototype.isPrototypeOf(beagle);  // yields true
+
+// Fix the code below so that it evaluates to true
+Object.prototype.isPrototypeOf(Dog.prototype)
