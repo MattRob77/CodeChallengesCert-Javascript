e16bfab748bad5b44d66c0d6d24cdb7d8682d28a
MattRob77 <Matt_Robinson1@yahoo.com>
2020-07-06 12:44:20 -0400

    Implemented solution.

--- OOP/Inherit Behaviors from a Supertype.js
+++ OOP/Inherit Behaviors from a Supertype.js
@@ -32,3 +32,16 @@ let beagle; // Change this line
 
 
 solution:
+function Animal() { }
+
+Animal.prototype = {
+  constructor: Animal,
+  eat: function() {
+    console.log("nom nom nom");
+  }
+};
+
+// Only change code below this line
+
+let duck = Object.create(Animal.prototype);
+let beagle = Object.create(Animal.prototype);
