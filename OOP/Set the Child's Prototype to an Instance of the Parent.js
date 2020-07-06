5e915a64f1419b547462e6629f777bb6f0d2e850
MattRob77 <Matt_Robinson1@yahoo.com>
2020-07-06 12:49:10 -0400

    Implemented solution for coding challenge.

--- OOP/Set the Child's Prototype to an Instance of the Parent.js
+++ OOP/Set the Child's Prototype to an Instance of the Parent.js
@@ -29,3 +29,16 @@ let beagle = new Dog();
 
 
 solution:
+function Animal() { }
+
+Animal.prototype = {
+  constructor: Animal,
+  eat: function() {
+    console.log("nom nom nom");
+  }
+};
+
+function Dog() { }
+
+Dog.prototype = Object.create(Animal.prototype)
+let beagle = new Dog();
